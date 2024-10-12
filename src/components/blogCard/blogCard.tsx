46444974c011cba66035e4303296a6c51b2c8c63
frankzhang <frank.zhang.sa.au@gmail.com>
2024-10-12 19:16:21 +1000

    1. use mock data in post page: A: BlogCard pass post B: in BlogCard: { post }---Parsing out parameters C: in BlogCard: { post: MockPostInterface }---Specifying parameter types

--- src/components/blogCard/blogCard.tsx
+++ src/components/blogCard/blogCard.tsx
@@ -2,14 +2,27 @@ import React from "react";
 import styles from "./blogCard.module.css";
 import Image from "next/image";
 import Link from "next/link";
-const BlogCard = () => {
+import { MockPostInterface } from "@/types";
+{
+  /* Step 2: { post }---Parsing out parameters */
+}
+{
+  /* Step 3: { post: MockPostInterface }---Specifying parameter types */
+}
+const BlogCard = ({ post }: { post: MockPostInterface }) => {
+  // console.log("post: ", post);
+  const { id, title, desc, userId, slug, img } = post;
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.imgContainer}>
           <Image
             className={styles.img}
-            src="https://images.pexels.com/photos/27947532/pexels-photo-27947532/free-photo-of-woman-with-food-on-a-picnic.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            src={
+              img != null
+                ? img
+                : "https://images.pexels.com/photos/27947532/pexels-photo-27947532/free-photo-of-woman-with-food-on-a-picnic.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
             alt={""}
             fill
           />
@@ -17,9 +30,9 @@ const BlogCard = () => {
         <span className={styles.date}>01.01.2024</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Hello world</h1>
-        <p className={styles.desc}>desc</p>
-        <Link href={"/blog/post"} className={styles.link}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.desc}>{desc}</p>
+        <Link href={`/blog/${id}`} className={styles.link}>
           READ MORE
         </Link>
       </div>
